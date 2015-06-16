b982e453ac1c55c67922d1dcacff0cfff605d10d
Soun-Ja Walters <sounjawalters2@gmail.com>
2015-06-16 16:15:54 -0500

    practice with if/else statements

--- public/js/external.js
+++ public/js/external.js
@@ -1,3 +1,17 @@
 // JavaScript goes here
 
 console.log("Hello from external JavaScript");
+
+var someBoolean = true;
+
+if(someBoolean) {
+	console.log("This variable is true");
+}
+
+var someBoolean = false;
+
+if(someBoolean){
+	console.log("This variable is" + someBoolean);
+} else {
+	
+}
